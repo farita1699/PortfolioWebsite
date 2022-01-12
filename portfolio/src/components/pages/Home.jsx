576e2e0082cb7e3b924f65c48afd1f4d0e477b22
farita1699 <uwanjerry25@gmail.com>
2022-01-12 10:56:56 -0500

    Test change

--- portfolio/src/components/pages/Home.jsx
+++ portfolio/src/components/pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Intro from "../intro/Intro"
 import Portfolio from "../portfolio/Portfolio"
-import Contact from "../contact/Contact"
-import Works from '../works/Works'
 import "./Home.scss"
 
 export default function Home() {
